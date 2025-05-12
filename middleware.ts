58bed7d99ff639e0cd1bc53f8083a6b848e4b9bb
Mohitk786 <kumarmohit08004@gmail.com>
2025-05-12 20:31:01 +0530

    minor fixes

--- middleware.ts
+++ middleware.ts
@@ -2,14 +2,17 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export async function middleware(req: NextRequest) {
-  const token = req.cookies.get("clg_app_cookie")?.value;
-  const isOnboardingPage = req.nextUrl.pathname === "/onboarding";
+  const token = await req.cookies.get("clg_app_cookie")?.value;
+  const path = req.nextUrl.pathname;
 
-  if (isOnboardingPage && token) {
+  const isOnboardingPage = path === "/onboarding";
+  const isProtectedRoute = ["/", "/confessions", "/campus-corner"].includes(path);
+
+  if (token && isOnboardingPage) {
     return NextResponse.redirect(new URL("/", req.url));
   }
 
-  if (!token && !isOnboardingPage) {
+  if (!token && isProtectedRoute) {
     return NextResponse.redirect(new URL("/onboarding", req.url));
   }
 
@@ -17,5 +20,5 @@ export async function middleware(req: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/", "/confessions", "/campus-corner", "/onboarding"],
+  matcher: ["/", "/confessions", "/campus-corner", "/onboarding"], 
 };
