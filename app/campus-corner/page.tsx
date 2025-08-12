5e9577f5b5620001a76b7bdcbed285e38323a899
Mohit Kumar <kumarmohit08004@gmail.com>
2025-08-12 09:01:29 +0530

    minor fixes

--- app/campus-corner/page.tsx
+++ app/campus-corner/page.tsx
@@ -4,12 +4,9 @@ import React from 'react'
 export const dynamic = "force-dynamic";
 
 const page = async () => {
-  
 
-  return <PostFeedPage type='news'
-    title='News'
-  />
-  
+  return <PostFeedPage type='news' title='News' />;
+
 }
 
 export default page
--- app/notifications/page.tsx
+++ app/notifications/page.tsx
@@ -1,4 +1,4 @@
-export const revalidate = 60 
+export const dynamic = "force-dynamic";
 
 import React from "react";
 import { formatDistanceToNow } from "date-fns";
@@ -29,7 +29,6 @@ const iconMap: Record<string, React.ReactNode> = {
 
 const NotificationList = async () => {
   const notifications = await getNotifications();
-  console.log("notifications", notifications);
 
   return (
     <div className="max-w-2xl mx-auto p-4 space-y-4">
